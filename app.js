60f9a7887f43fb30fbf913fe9708114e371d7db4
kimjinwoo <superlucky84@gmail.com>
2016-02-22 01:01:20 +0900

    * ADD REDUX file and indexcontroller

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
 var http = require('http');
 var debug = require('debug')('sorting:server');
 
+
+
 // ROUTING
-var jnotecontrol = require('./controller/JnoteController.js');
+var jnotecontrol = require('./controller/JnoteController');
+var indexcontrol = require('./controller/IndexController');
 
 
 
@@ -15,6 +20,23 @@ var jnotecontrol = require('./controller/JnoteController.js');
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+//
+app.use('/dist', express.static(__dirname + '/dist'));
+
+
+//app.use(express.static('dist'));
+
+console.log('kk');
+console.log(path.join(__dirname, 'dist'));
+
+app.use('/', indexcontrol);
 app.use('/jnote', jnotecontrol);
 
 

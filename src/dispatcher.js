7ea446875e1d70e16a1d291fa00b66747a825a23
superlucky84 <superlucky84@gmail.com>
2019-10-17 21:45:49 +0900

    Update dispatcher.js

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -89,7 +89,7 @@ export default async function dispatcher(action) {
     });
 
     action.idx = choiceTarget;
-    action.data = updateResult;
+    action.data = updateResult;mmmmmmmmmmmmmm
   }
 
   mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
